14a2eafd88cf44d2d59d3e4a88d70144ea92ad5c
Frederikke Drejer Therkildsen <fthe@itu.dk>
2021-10-04 12:39:28 +0100

    første skitse på buttons

--- src/components/Navbar/Navigationbar.js
+++ src/components/Navbar/Navigationbar.js
@@ -1,30 +1,26 @@
 import React from 'react';
-import {Navbar, Nav, Button, Container} from 'react-bootstrap';
+import {Button, Container, Col} from 'react-bootstrap';
 import Logo from "../../images/logo-prelogin.png";
 import "./Navigationbar.css";
+import 'bootstrap/dist/css/bootstrap.css';
 
 
 export default function LandingPage(){
 
     return(
-        <Navbar className="navbar" expand="lg" sticky="top">
-            
-            <Container>
-                <Navbar.Brand className="navbar-brand">Maths Camp</Navbar.Brand>
-            </Container>
-
-            <Container>
-                <Nav>
-                    <img className="logo" src={Logo} alt="Logo of a calculator"></img>   
-                </Nav>
-            </Container>
-
-            <Container className="btn-container">
-                <Nav>
-                    <Button className="home-btn">Home</Button>
-                    <Button className="logout-btn">Log out</Button>
-                </Nav>
-            </Container>       
-        </Navbar>
+        <Container className="navbar">
+            <Col>
+                <img className="logo" src={Logo} alt="Logo of a calculator"></img>   
+            </Col>
+            <Col className="app-name-container">
+                <h5 className="navbar-brand">Maths Camp</h5>
+            </Col>
+            <Col lg={2} className="btn-container">
+                <div className="btn-toolbar">
+                    <Button className="p-2 btn-primary lg home-btn">Home</Button>
+                    <Button className="p-2 btn-primary lg logout-btn">Log out</Button>
+                </div>
+            </Col>
+        </Container>       
     );
 }
