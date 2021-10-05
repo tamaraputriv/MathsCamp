57de848f8738fb1648e1fe031c932f7a7b815a20
anneschjoedt <anne.schjoedt@gmail.com>
2021-10-05 12:12:00 +0200

    Sidebar and icons

---
+++ src/pages/HomePage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Sidebar from "../components/Sidebar/Sidebar";
+
+const HomePage = () => {
+  return (
+    <>
+      <Sidebar />
+    </>
+  );
+};
+
+export default HomePage;
