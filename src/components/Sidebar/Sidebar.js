e6f2f0d2f466af95f4d63add54177c3fcae95d13
anneschjoedt <anne.schjoedt@gmail.com>
2021-10-05 14:47:29 +0200

    Sidebar transistion

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./Sidebar.css";
-import { BsChevronDoubleRight } from "react-icons/bs";
+import { BsChevronDoubleRight, BsChevronDoubleLeft } from "react-icons/bs";
 import UnlockedBagde from "../../images/Icons/unlocked-reward-badge.svg";
 import { Container, Col, Row } from "react-bootstrap";
 
@@ -8,32 +8,50 @@ export default function Sidebar({ isOpen, toggle }) {
   return (
     <Container
       className="sidebar-container"
-      style={{ width: isOpen ? "35%" : "10%" }}
+      style={{ width: isOpen ? "" : "15%", height: isOpen ? "100vh" : "100%" }}
     >
       <Row className="icon-row">
         <Col></Col>
         <Col md="auto">
-          <BsChevronDoubleRight
-            className="arrow-icon"
-            alt="icon arrow"
-          ></BsChevronDoubleRight>
+          {isOpen ? (
+            <BsChevronDoubleLeft
+              onClick={toggle}
+              className="arrow-icon"
+              alt="icon arrow"
+            ></BsChevronDoubleLeft>
+          ) : (
+            <BsChevronDoubleRight
+              onClick={toggle}
+              className="arrow-icon"
+              alt="icon arrow"
+            ></BsChevronDoubleRight>
+          )}
         </Col>
       </Row>
       <Row className="h1-row">
         <Col md="auto">
-          <h1 className="sidebarH1">Your Collection</h1>
+          <h1
+            className="sidebarH1"
+            style={{
+              fontSize: isOpen ? "" : "20px",
+              marginTop: isOpen ? "" : "0",
+              marginBottom: isOpen ? "" : "0",
+            }}
+          >
+            Your Collection
+          </h1>
         </Col>
       </Row>
       <Row>
         <Col>
-          <p className="sidebarP">
+          <p className="sidebarP" style={{ display: isOpen ? "" : "none" }}>
             Your collection is empty! Go to Practice mode and earn your first
             reward.
           </p>
         </Col>
       </Row>
       <Row className="badge-row">
-        <Col>
+        <Col className="badge-col">
           <img
             className="unlocked-badge"
             src={UnlockedBagde}
