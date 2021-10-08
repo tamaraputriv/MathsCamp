966c35d6eab1396b5dfcaa459ce207f097fb1ebb
anneschjoedt <anne.schjoedt@gmail.com>
2021-10-08 13:41:25 +0200

    sticky nav

--- src/components/Navbar/NavbarPostlogin.js
+++ src/components/Navbar/NavbarPostlogin.js
@@ -7,29 +7,31 @@ import { DoorOpen, HouseDoor } from "react-bootstrap-icons";
 
 export default function Navbar_postlogin() {
   return (
-    <Container fluid className="navbar-postlogin">
-      <Row>
-        <Col>
-          <img
-            className="logo-postlogin"
-            src={Logo}
-            alt="Logo of a calculator"
-          ></img>
-        </Col>
-        <Col className="app-name-col-postlogin">
-          <h6 className="navbar-brand-postlogin">Maths Camp</h6>
-        </Col>
-        <Col>
-          <div className="btn-toolbar postlogin-toolbar">
-            <Button className="btn-primary lg home-btn-postlogin">
-              Home <HouseDoor size={15} />
-            </Button>
-            <Button className="btn-primary lg logout-btn-postlogin">
-              Log out <DoorOpen size={15} />
-            </Button>
-          </div>
-        </Col>
-      </Row>
-    </Container>
+    <div className="nav">
+      <Container fluid className="navbar-postlogin">
+        <Row>
+          <Col>
+            <img
+              className="logo-postlogin"
+              src={Logo}
+              alt="Logo of a calculator"
+            ></img>
+          </Col>
+          <Col className="app-name-col-postlogin">
+            <h6 className="navbar-brand-postlogin">Maths Camp</h6>
+          </Col>
+          <Col>
+            <div className="btn-toolbar postlogin-toolbar">
+              <Button className="btn-primary lg home-btn-postlogin">
+                Home <HouseDoor size={15} />
+              </Button>
+              <Button className="btn-primary lg logout-btn-postlogin">
+                Log out <DoorOpen size={15} />
+              </Button>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </div>
   );
 }
