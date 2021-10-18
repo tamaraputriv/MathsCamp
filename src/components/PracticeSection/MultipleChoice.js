a2b1ebe42d245944d56a47fa2d7c4776a94b28dd
anneschjoedt <anne.schjoedt@gmail.com>
2021-10-18 15:56:30 +0200

    retrieve question + answer check

--- src/components/PracticeSection/MultipleChoice.js
+++ src/components/PracticeSection/MultipleChoice.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import Parse from "parse";
 import {
   Container,
   Row,
@@ -13,31 +14,77 @@ import { BsLifePreserver, BsCheckCircle } from "react-icons/bs";
 import Mascot from "../../images/Mascots/mascot1.png";
 import SpeakBoble from "../../images/Icons/SpeakBoble.svg";
 
-export default function MultipleChoice({
-  question,
-  category,
-  option_A,
-  option_B,
-  option_C,
-  option_D,
-  option_E,
-  correct_answer,
-  hint,
-  imgsrc,
-}) {
+export default function MultipleChoice() {
   const [showHint, setShowHint] = useState(false);
+  const [description, setDescription] = useState("");
+  const [options, setOptions] = useState([]);
+  const [chosenOption, setChosenOption] = useState("");
+  const [correct_answer, setCorrectAnswer] = useState("");
+  const [hint, setHint] = useState("");
+  const [image, setImage] = useState("");
+
+  const retrieveQuestion = async () => {
+    const Question = Parse.Object.extend("Questions");
+    const query = new Parse.Query(Question);
+
+    query.get("61w4Jy1Wc0").then(
+      (question) => {
+        var description = question.get("description");
+        var option_1 = question.get("option_1");
+        var option_2 = question.get("option_2");
+        var option_3 = question.get("option_3");
+        var option_4 = question.get("option_4");
+        var option_5 = question.get("option_5");
+        var correct_answer = question.get("correct_answer");
+        var hint = question.get("hint");
+        var image = question.get("img_src");
+        setDescription(description);
+        setOptions(
+          options.concat(option_1, option_2, option_3, option_4, option_5)
+        );
+        setHint(hint);
+        setImage(image);
+        setCorrectAnswer(correct_answer);
+      },
+      (error) => {
+        // The object was not retrieved successfully.
+        // error is a Parse.Error with an error code and message.
+        alert(
+          "Failed to retrieve the object, with error code: " + error.message
+        );
+      }
+    );
+  };
+
+  const handleChange = (e) => {
+    e.persist();
+    console.log(e.target.value);
+    setChosenOption(e.target.value);
+  };
+
+  const handleSubmit = () => {
+    if (correct_answer === chosenOption) {
+      alert("the answer is correct!");
+    } else {
+      alert("the answer is NOT correct!");
+    }
+  };
+
+  useEffect(() => {
+    retrieveQuestion();
+  }, []);
 
   return (
     <Container fluid className="multiple-container">
       <Row>
         <Col md="auto" className="question-img-col">
-          <Image />
+          <Image src={image} />
         </Col>
         <Col className="question-col">
           <Card className="title-card">
             <Card.Body className="text-center">
               <Card.Title className="question-description">
-                {question}
+                {description}
               </Card.Title>
             </Card.Body>
           </Card>
@@ -46,51 +93,36 @@ export default function MultipleChoice({
               <Card.Body className="text-center">
                 <fieldset className="options-form">
                   <Form.Group as={Row}>
-                    <Col sm={0}>
-                      <Form.Check
-                        type="radio"
-                        value={option_A}
-                        label={`${option_A}`}
-                        name="formHorizontalRadios"
-                        id="option1"
-                        className="option-text"
-                      />
-                      <Form.Check
-                        type="radio"
-                        label={`${option_B}`}
-                        name="formHorizontalRadios"
-                        id="option2"
-                      />
-                      <Form.Check
-                        type="radio"
-                        label={`${option_C}`}
-                        name="formHorizontalRadios"
-                        id="option3"
-                      />
-                      <Form.Check
-                        type="radio"
-                        label={`${option_D}`}
-                        name="formHorizontalRadios"
-                        id="option4"
-                      />
-                      <Form.Check
-                        type="radio"
-                        label={`${option_E}`}
-                        name="formHorizontalRadios"
-                        id="option5"
-                      />
+                    <Col className="options">
+                      {options.map((option) => (
+                        <div key={`${option}`}>
+                          <Form.Check
+                            type="radio"
+                            value={option}
+                            label={`${option}`}
+                            name="formHorizontalRadios"
+                            className="option-text"
+                            onChange={handleChange}
+                          />
+                        </div>
+                      ))}
                     </Col>
                   </Form.Group>
                 </fieldset>
               </Card.Body>
             </Card>
+
             <Form.Group as={Row} className="mb-8 mt-8">
               <div className="btn-div">
                 <Button className="hint-btn quiz-btn" onClick={setShowHint}>
                   Hint
                   <BsLifePreserver className="btn-icon" />
                 </Button>
-                <Button className="submit-btn quiz-btn" type="submit">
+                <Button
+                  className="submit-btn quiz-btn"
+                  onClick={handleSubmit}
+                  type="submit"
+                >
                   Submit <BsCheckCircle className="btn-icon" />
                 </Button>
               </div>
