698adf8a7cee00ec96b4fd6e705b090a43065105
anneschjoedt <anne.schjoedt@gmail.com>
2021-10-26 09:46:35 +0200

    deleted user object

--- src/pages/FrontPage.js
+++ src/pages/FrontPage.js
@@ -1,13 +1,12 @@
 import React from "react";
 import Navbar from "../components/Navbar/NavbarPostlogin";
 import UserInfo from "../components/UserInfo/UserInfo";
-import { User } from "../users/User";
 
 export default function FrontPage() {
   return (
     <div>
       <Navbar />
-      <UserInfo {...User} />
+      <UserInfo />
     </div>
   );
 }
