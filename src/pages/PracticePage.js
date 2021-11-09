53d0718ea22a99d088c7ee7c0ca26c996529b102
anneschjoedt <anne.schjoedt@gmail.com>
2021-11-09 21:21:33 +0100

    only multiple

--- src/pages/PracticePage.js
+++ src/pages/PracticePage.js
@@ -3,16 +3,13 @@ import Navbar from "../components/Navbar/NavbarPracticeMode";
 import MultipleChoice from "../components/PracticeSection/MultipleChoice";
 import Input from "../components/PracticeSection/Input";
 import { Question, QuestionInput } from "../questions/Question";
+import Footer from "../components/Footer/Footer";
 
 export default function PracticePage() {
   return (
     <div>
       <Navbar />
-      {Question.type === 1 ? (
-        <MultipleChoice {...Question} />
-      ) : (
-        <Input {...QuestionInput} />
-      )}
+      <MultipleChoice />
     </div>
   );
 }
