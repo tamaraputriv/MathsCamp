09bfe45cb7e686f5864bc5e6af8dfd3c8e3f3e8c
anneschjoedt <anne.schjoedt@gmail.com>
2021-11-10 22:54:26 +0100

    footer design

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,6 +2,7 @@ import React from "react";
 import { animateScroll as scroll } from "react-scroll";
 import { useHistory } from "react-router";
 import Logo from "../../images/Logo/logo-prelogin.svg";
+import { Link } from "react-router-dom";
 import "./Footer.css";
 import {
   Container,
@@ -14,36 +15,39 @@ import {
 } from "react-bootstrap";
 
 const Footer = () => {
-  const history = useHistory();
-  //Redirects the user to the frontpage
-  const handleHome = () => {
-    history.push("/frontpage");
-  };
-  const toggleHome = () => {
+  const scrollToTop = () => {
     scroll.scrollToTop();
   };
+
+  const ButtonMailto = ({ mailto, label }) => {
+    return (
+      <Button
+        className="contact-link contact-btn"
+        to="#"
+        onClick={(e) => {
+          window.location = mailto;
+          e.preventDefault();
+        }}
+      >
+        {label}
+      </Button>
+    );
+  };
+
   return (
     <div className="footer-c">
-      <div className="logo-c">
-        <img
-          className="footer-logo"
-          src={Logo}
-          alt="Logo of a calculator"
-          onClick={handleHome}
-        ></img>
-        <h6 className="logo-name">Maths Camp</h6>
+      <div className="row-1">
+        <ButtonMailto label="Contact us" mailto="mailto:no-reply@example.com" />
+        <Button className="contact-btn">About us</Button>
+        <Button className="contact-btn">Join us</Button>
       </div>
-      <div className="rights-c">
-        <p className="rights">
-          ©2021 All rights reserved. Maths Camp is created by Anne Schjødt and
-          Frederikke Drejer in collaboration with IT University of Copenhagen.
-        </p>
-      </div>
-      <div className="contact-c">
-        <h6 className="logo-name">Contact us</h6>
-        <a href="mailto:someone@example.com">
-          <p className="email-link">example@itu.dk</p>
-        </a>
+      <div className="row-2">
+        <div className="rights-c">
+          <p className="rights">
+            ©2021 All rights reserved. Maths Camp is created by Anne Schjødt and
+            Frederikke Drejer in collaboration with IT University of Copenhagen.
+          </p>
+        </div>
       </div>
     </div>
   );
