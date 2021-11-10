a4e71d0310c12bfc16e6751bd1d28ceefbab7c7d
anneschjoedt <anne.schjoedt@gmail.com>
2021-11-10 09:52:24 +0100

    deleted import

--- src/pages/PracticePage.js
+++ src/pages/PracticePage.js
@@ -1,9 +1,6 @@
 import React from "react";
 import Navbar from "../components/Navbar/NavbarPracticeMode";
 import MultipleChoice from "../components/PracticeSection/MultipleChoice";
-import Input from "../components/PracticeSection/Input";
-import { Question, QuestionInput } from "../questions/Question";
-import Footer from "../components/Footer/Footer";
 
 export default function PracticePage() {
   return (
