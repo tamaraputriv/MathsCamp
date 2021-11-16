1b2d42ab8ad78c865737c2a72b17e7cd272717ac
anneschjoedt <anne.schjoedt@gmail.com>
2021-11-16 13:03:35 +0100

    working contactPage

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -39,53 +39,38 @@ export default function Contact() {
     <Container fluid className="email-container">
       <Row className="justify-content-md-center email-row">
         <Col sx md="8" lg="5" className="email-col">
-          <div className="text-center">
+          <div className="text-center email-text-div">
             <Tree size={30} color="#4D4D4D" />
             <h1 className="welcome-h1">Send us an email</h1>
             <p className="welcome-p">We would love to hear from you</p>
           </div>
           <Form ref={form} action="#" onSubmit={sendEmail}>
             <Form.Group className="mb-3">
-              <FloatingLabel
-                controlId="floatingInput"
-                label="Name"
-                className="mb-3"
-              >
-                <Form.Control
-                  type="text"
-                  name="user_name"
-                  placeholder="Name"
-                  size="lg"
-                />
-              </FloatingLabel>
+              <Form.Control
+                type="text"
+                name="user_name"
+                placeholder="Your name"
+                size="lg"
+              />
             </Form.Group>
             <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-              <FloatingLabel
-                controlId="floatingInput"
-                label="Email"
-                className="mb-3"
-              >
-                <Form.Control
-                  type="email"
-                  placeholder="name@example.com"
-                  size="lg"
-                />
-              </FloatingLabel>
+              <Form.Control
+                type="email"
+                name="user_email"
+                placeholder="your@email.com"
+                size="lg"
+              />
             </Form.Group>
             <Form.Group
               className="mb-3"
               controlId="exampleForm.ControlTextarea1"
             >
-              <FloatingLabel
-                controlId="floatingTextarea2"
-                label="Send us a message here :)"
-              >
-                <Form.Control
-                  as="textarea"
-                  placeholder="Leave a comment here"
-                  style={{ height: "150px" }}
-                />
-              </FloatingLabel>
+              <Form.Control
+                as="textarea"
+                name="message"
+                placeholder="Send us a message here :)"
+                style={{ height: "150px" }}
+              />
             </Form.Group>
             <div className="btn-div">
               <Button
