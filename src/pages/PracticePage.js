ff2d0ea058e043ba8eb7576ba8b5a4e086f0a644
anneschjoedt <anne.schjoedt@gmail.com>
2021-11-16 13:04:13 +0100

    h1 + p className

--- src/pages/PracticePage.js
+++ src/pages/PracticePage.js
@@ -1,19 +1,12 @@
 import React from "react";
 import Navbar from "../components/Navbar/NavbarPracticeMode";
 import MultipleChoice from "../components/PracticeSection/MultipleChoice";
-import Footer from "../components/Footer/Footer";
 
 export default function PracticePage() {
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "space-between",
-      }}
-    >
+    <>
       <Navbar />
       <MultipleChoice />
-    </div>
+    </>
   );
 }
