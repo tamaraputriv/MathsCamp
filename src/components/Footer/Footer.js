117b511ec8b0f3a836e119faf2a11a083e69f76f
anneschjoedt <anne.schjoedt@gmail.com>
2021-11-20 11:49:02 +0100

    clean-up

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,7 +1,5 @@
 import React from "react";
-import { useHistory } from "react-router";
 import "./Footer.css";
-import { Button } from "react-bootstrap";
 
 const Footer = () => {
   return (
--- src/components/PracticeSection/MultipleChoice.js
+++ src/components/PracticeSection/MultipleChoice.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect } from "react";
 import Parse from "parse";
 import Swal from "sweetalert2";
 import {
