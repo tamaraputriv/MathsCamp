9bac44a69eea0411fcead7c733004abdb128e628
Mia Rønnelund <miro@itu.dk>
2022-03-29 21:21:11 +0200

    fetching classroom+students from DB

--- src/components/Ranking/Ranking.js
+++ src/components/Ranking/Ranking.js
@@ -1,38 +1,14 @@
 import React, { useEffect, useState } from "react";
-// import { Card, Button, Container, Row, Col } from "react-bootstrap";
 import Parse from "parse";
-// import UserInfoTable from "../UserInfoTable/UserInfoTable";
 import Leaderboard from "../LeaderboardTable/LeaderboardTable";
 import "./Ranking.css";
-import { async } from "parse/lib/browser/Storage";
 
 export default function MyPage() {
-  const [username, setUsername] = useState("");
-  const [points, setPoints] = useState(0);
-  const [active_days, set_active_days] = useState([]);
-  const [total_answered_questions, setTotal_answered_questions] = useState(0);
-  const [current_mascot, setCurrentMascot] = useState("");
-
-  const retrieveStudent = async (e) => {
-    const student = Parse.User.current();
-    if (student) {
-      const name = student.get("username");
-      const mascot = student.get("active_mascot_id");
-      const userpoints = student.get("total_points");
-      const active_days = student.get("active_days");
-      const total_answered_questions = student.get("total_answered_questions");
-      setPoints(userpoints);
-      setUsername(name);
-      set_active_days(active_days);
-      setTotal_answered_questions(total_answered_questions);
-      setCurrentMascot(mascot);
-    } else {
-      e.preventDefault();
-    }
-  };
+  const [students_info, setStudentInfo] = useState([]);
+  const numbers = [1, 2, 3, 4, 5];
 
   const fetchClass = async (e) => {
-    const student_info = [];
+    let student_info = [];
 
     const user = Parse.User.current();
     const classroom = Parse.Object.extend("Classroom");
@@ -45,53 +21,67 @@ export default function MyPage() {
       .then((object) => {
         const studentids = object.get("students");
         const students = Parse.Object.extend("User");
-        const q_student = new Parse.Query(students);
 
-        studentids.forEach((student) => {
-          console.log(student);
+        for (let i = 0; i < studentids.length; i++) {
+          const q_student = new Parse.Query(students);
           q_student
-            .get(student)
-            .then((stud) => {
-              student_info.push(stud.get("username"));
+            .get(studentids[i])
+            .then((student) => {
+              const name = student.get("username");
+              const mascotId = student.get("active_mascot_id");
+              const points = student.get("total_points");
+
+              student_info.push({
+                name: name,
+                mascotid: mascotId,
+                points: points,
+              });
             })
             .catch((err) => {
               console.log(err);
             });
-        });
+        }
+        setStudentInfo(student_info);
       })
       .catch((error) => {
         console.log(error);
       });
 
-    console.log(student_info);
-  };
-
-  const Ranking = async (e) => {
-    // const Users = new Parse.Object.extend("User");
-    // const q = new Parse.Query(Users).addDescending("total_points");
-    // const user_array = await q.find();
-    // return user_array;
+    student_info.sort((a, b) => (b.points > a.points ? 1 : -1));
   };
 
   useEffect(() => {
     fetchClass();
+    document.title = "This is the student ${students_info}";
   }, []);
 
   return (
     <div className="user-container">
       <div className="table-div">
-        <Leaderboard
-          rank={25}
-          username={username}
-          current_mascot={current_mascot}
-          total_points={points}
-        />
-        {/* <UserInfoTable
-          total_points={points}
-          active_days={active_days.length}
-          total_answered_questions={total_answered_questions}
-          ranking={"25"}
-        /> */}
+        <ul>
+          {/* {students_info.map((student, index) => (
+            <Leaderboard key={index} rank={student.points} />
+          ))} */}
+          {students_info.map((student, index) => (
+            <p key={index}>{student.name}</p>
+          ))}
+
+          {/* <Leaderboard
+            rank={25}
+            username={"hello"}
+            current_mascot={"klafsdj"}
+            total_points={2500}
+          /> */}
+          {/* {students_info.map((student, index) => (
+            <Leaderboard
+              key={index}
+              rank={student.index}
+              username={student.name}
+              current_mascot={student.mascotId}
+              total_points={student.points}
+            />
+          ))} */}
+        </ul>
       </div>
     </div>
   );
