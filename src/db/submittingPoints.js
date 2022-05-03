250d577e059d83a69a00687e50e6430649e7ce05
Mia Rønnelund <miro@itu.dk>
2022-05-03 10:36:00 +0200

    Ensuring same date register for all users

--- src/db/submittingPoints.js
+++ src/db/submittingPoints.js
@@ -1,7 +1,7 @@
 import Parse from "parse";
 
 async function registerPoints(studentID, points) {
-  const date = new Date().toLocaleDateString();
+  const date = new Date().toLocaleDateString("dk");
   const point_schema = new Parse.Object("Points");
 
   point_schema.set("User_id", studentID);
